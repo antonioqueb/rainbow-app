617e843119c0a8f3f3a97ff2b90970de63e2e359
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-01-05 23:13:55 -0600

    Update colors and styles in CSS files

--- app/contacto/page.js
+++ app/contacto/page.js
@@ -1,9 +1,30 @@
-
-
+import styles from './contacto.module.css';
 
 export default function ContactPage() {
     return (
-     <h1>Contacto Page</h1>
+        <>
+        <section className={styles.contacto}>
+           <form className={styles.form}>
+            <div className={styles.formGroupTitle}>
+                <h1>Contacto</h1>
+                <p>Si tienes alguna duda o sugerencia, no dudes en contactarnos.</p>
+            </div>
+                <div className={styles.formGroup}>
+                    <input type="text" placeholder="Nombre" />
+                    <input type="email" placeholder="Apellido" />
+                </div>
+                <div className={styles.formGroup}>
+                    <input type="email" placeholder="Email" />
+                    <input type="email" placeholder="Teléfono" />
+                </div>
+                <div className={styles.buttomSection}>
+                <button className={styles.button}>Enviar</button>
+                </div>
+
+            </form>
+        </section>
+        </>
     )
-  }
+}
+
   
