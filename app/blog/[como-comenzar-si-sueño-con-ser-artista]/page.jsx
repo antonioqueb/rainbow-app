b9d68e8e0958e6c66f4c7c7c8977e6c2db0490d7
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-01-31 13:40:31 -0600

    Agrega atributos de ancho y alto a la imagen de portada en la página de blog

--- app/blog/[como-comenzar-si-sueño-con-ser-artista]/page.jsx
+++ app/blog/[como-comenzar-si-sueño-con-ser-artista]/page.jsx
@@ -12,8 +12,7 @@ export const PostOne = () => {
         
       </header>
       <div className={styles['post_image']}>
-        <Image src='/cover/1.jpg' alt='cover'   
-            />
+        <Image src='/cover/1.jpg' alt='cover' width={1080} height={720}  className={styles['post_image__img']} />
       </div>
       <article className={styles['post_article']}>
         <p className={styles['post_paragraph']}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquet enim tortor at auctor urna nunc id cursus. Quis varius quam quisque id. Lobortis mattis aliquam faucibus purus in. Id neque aliquam vestibulum morbi. Tristique senectus et netus et malesuada fames ac turpis egestas. Enim lobortis scelerisque fermentum dui faucibus in ornare quam. Nulla facilisi morbi tempus iaculis urna id. Turpis massa sed elementum tempus egestas. Fusce ut placerat orci nulla pellentesque. Nam aliquam sem et tortor. Purus sit amet volutpat consequat. Ac turpis egestas maecenas pharetra convallis posuere morbi leo. Nulla pellentesque dignissim enim sit amet venenatis urna cursus. Vitae elementum curabitur vitae nunc sed velit dignissim sodales. Mauris augue neque gravida in fermentum et sollicitudin ac orci.
