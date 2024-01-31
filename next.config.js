75733d474e98ce23423f52993b98e403dec4ac47
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-01-30 21:59:56 -0600

    Agrega configuraciones de imágenes en next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    images: {
+      dangerouslyAllowSVG: true,
+      contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    },
+    // ... puedes agregar más configuraciones aquí si necesitas
+  }
+  
+  module.exports = nextConfig
+  
