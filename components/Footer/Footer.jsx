0b1542f12f8e7ffb38ac3322056cb3f1c7370a0e
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-01-31 17:12:32 -0600

    Actualizar estilos de CSS en componentes CardItemArtistProfile y CardProfile, y corregir enlaces en Footer

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -18,22 +18,22 @@ export const Footer = () => {
             <h4 className={styles.Footer__container__footer__links__title}>ARK PROJECT</h4>
             <ul className={styles.Footer__container__footer__links__list}>
               <li className={styles.Footer__container__footer__links__list__item}>
-                <Link href="/about">
+                <Link href="/nosotros" style={{ textDecoration: 'none'}}>
                   <span className={styles.Footer__container__footer__links__list__item__link}>¿Porqué Nosotros?</span>
                 </Link>
               </li>
               <li className={styles.Footer__container__footer__links__list__item}>
-                <Link href="/services">
-                  <span className={styles.Footer__container__footer__links__list__item__link}>Servicios</span>
+                <Link href="/services" style={{ textDecoration: 'none'}}>
+                  <span className={styles.Footer__container__footer__links__list__item__link}>Contacto</span>
                 </Link>
               </li>
               <li className={styles.Footer__container__footer__links__list__item}>
-                <Link href="/tools">
+                <Link href="/tools" style={{ textDecoration: 'none'}}>
                   <span className={styles.Footer__container__footer__links__list__item__link}>Herramientas</span>
                 </Link>
               </li>
               <li className={styles.Footer__container__footer__links__list__item}>
-                <Link href="/blog">
+                <Link href="/blog"  style={{ textDecoration: 'none'}}>
                   <span className={styles.Footer__container__footer__links__list__item__link}>Blog</span>
                 </Link>
               </li>
@@ -44,22 +44,22 @@ export const Footer = () => {
             <h4 className={styles.Footer__container__footer__links__title}>Ayuda</h4>
             <ul className={styles.Footer__container__footer__links__list}>
               <li className={styles.Footer__container__footer__links__list__item}>
-                <Link href="/about">
+                <Link href="/about" style={{ textDecoration: 'none'}}>
                   <span className={styles.Footer__container__footer__links__list__item__link}>¿Porqué Nosotros?</span>
                 </Link>
               </li>
               <li className={styles.Footer__container__footer__links__list__item}>
-                <Link href="/services">
+                <Link href="/services" style={{ textDecoration: 'none'}}>
                   <span className={styles.Footer__container__footer__links__list__item__link}>Servicios</span>
                 </Link>
               </li>
               <li className={styles.Footer__container__footer__links__list__item}>
-                <Link href="/tools">
+                <Link href="/tools" style={{ textDecoration: 'none'}}>
                   <span className={styles.Footer__container__footer__links__list__item__link}>Herramientas</span>
                 </Link>
               </li>
               <li className={styles.Footer__container__footer__links__list__item}>
-                <Link href="/blog">
+                <Link href="/blog" style={{ textDecoration: 'none'}}>
                   <span className={styles.Footer__container__footer__links__list__item__link}>Blog</span>
                 </Link>
               </li>
@@ -70,22 +70,22 @@ export const Footer = () => {
             <h4 className={styles.Footer__container__footer__links__title}>Preguntas</h4>
             <ul className={styles.Footer__container__footer__links__list}>
               <li className={styles.Footer__container__footer__links__list__item}>
-                <Link href="/about">
+                <Link href="/about" style={{ textDecoration: 'none'}}>
                   <span className={styles.Footer__container__footer__links__list__item__link}>¿Porqué Nosotros?</span>
                 </Link>
               </li>
               <li className={styles.Footer__container__footer__links__list__item}>
-                <Link href="/services">
+                <Link href="/services" style={{ textDecoration: 'none'}}>
                   <span className={styles.Footer__container__footer__links__list__item__link}>Servicios</span>
                 </Link>
               </li>
               <li className={styles.Footer__container__footer__links__list__item}>
-                <Link href="/tools">
+                <Link href="/tools" style={{ textDecoration: 'none'}}>
                   <span className={styles.Footer__container__footer__links__list__item__link}>Herramientas</span>
                 </Link>
               </li>
               <li className={styles.Footer__container__footer__links__list__item}>
-                <Link href="/blog">
+                <Link href="/blog" style={{ textDecoration: 'none'}}>
                   <span className={styles.Footer__container__footer__links__list__item__link}>Blog</span>
                 </Link>
               </li>
@@ -95,16 +95,16 @@ export const Footer = () => {
           <div className={styles.Footer__container__row__footer__links}>
             <h4 className={styles.Footer__container__footer__links__title}>Siguenos</h4>
             <div className={styles.Footer__container__footer__links__title__social__link}>
-              <Link href="/facebook">
+              <Link href="/facebook" style={{ textDecoration: 'none'}}>
                 <FaFacebook className={styles.Footer__container__footer__links__title__social__link__icon} />
               </Link>
-              <Link href="/twitter">
+              <Link href="/twitter" style={{ textDecoration: 'none'}}>
                 <FaXTwitter className={styles.Footer__container__footer__links__title__social__link__icon} />
               </Link>
-              <Link href="/instagram">
+              <Link href="/instagram" style={{ textDecoration: 'none'}}>
                 <FaInstagram className={styles.Footer__container__footer__links__title__social__link__icon} />
               </Link>
-              <Link href="/linkedin">
+              <Link href="/linkedin" style={{ textDecoration: 'none'}}>
                 <FaLinkedin className={styles.Footer__container__footer__links__title__social__link__icon} />
               </Link>
 
