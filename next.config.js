5e37d8d16b7e6a3820816deb39a8b735ad21a340
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-01-30 22:11:57 -0600

    Actualizar next.config.js para eliminar la configuración de imágenes peligrosas y agregar una configuración vacía.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      dangerouslyAllowSVG: true,
-      contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    },
-    // ... puedes agregar más configuraciones aquí si necesitas
-  }
-  
-  module.exports = nextConfig
-  
+const nextConfig = {}
+
+module.exports = nextConfig
