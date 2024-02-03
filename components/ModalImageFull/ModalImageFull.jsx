3c7f37f83d63ad06c706d1e4eaa122f436c0eed7
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-02-02 18:46:16 -0600

    Actualizar diseño de ModalImageFull y agregar comentarios ficticios

--- components/ModalImageFull/ModalImageFull.jsx
+++ components/ModalImageFull/ModalImageFull.jsx
@@ -9,12 +9,10 @@ export const ModalImageFull = () => {
 
 
 
-      {/* Botón de cierre en la esquina superior derecha */}
-      <div className={styles['ModalImageFull__section__close__modal']}>
-        <button className={styles['ModalImageFull__section__close__modal__button']}>
-          <AiOutlineFullscreenExit  className={styles['ModalImageFull__section__close__modal__button__icon']} />
-        </button>
-      </div>
+   
+
+
+
 
 
 
@@ -23,19 +21,31 @@ export const ModalImageFull = () => {
 
       {/* Contenido de la imagen a la izquierda */}
       <div className={styles['ModalImageFull__section__content']}>
-        <div className={styles['ModalImageFull__section__image']}>
+
+           {/* Botón de cierre en la esquina superior derecha */}
+      <div className={styles['ModalImageFull__section__close__modal']}>
+        <button className={styles['ModalImageFull__section__close__modal__button']}>
+          <AiOutlineFullscreenExit  className={styles['ModalImageFull__section__close__modal__button__icon']} />
+        </button>
+      </div>
+
           {/* Imagen de perfil */}
           <Image
-            src='/Trend/1.jpeg'
+            src='/Trend/3.jpeg'
             alt='perfil'
-            width={500}
-            height={500}
+            width={900}
+            height={900}
+            layout="fixed"
             className={styles['ModalImageFull__section__image__Image']}
           />
-        </div>
+        
       </div>
 
    
+
+
+
+
 
       {/* Seccion a la derecha */}
       <section className={styles['ModalImageFull__section__comments']}>
@@ -44,7 +54,7 @@ export const ModalImageFull = () => {
         <div className={styles['ModalImageFull__section__info__artist__text']}>
         <div className={styles['ModalImageFull__section__info__artist__avatar']}>
           {/* Imagen de perfil */}
-          <Image src='/artist/artist.jpeg' alt='perfil' width={50} height={50} className={styles['ModalImageFull__section__info__artist__avatar__img']} />
+          <Image src='/artist/artist.jpeg' alt='perfil' width={70} height={70} className={styles['ModalImageFull__section__info__artist__avatar__img']} />
         </div>
         <div className={styles['ModalImageFull__section__info__artist__name__and__username']}>
         <div className={styles['ModalImageFull__section__info__artist__name']}>
@@ -59,6 +69,11 @@ export const ModalImageFull = () => {
         <div className={styles['ModalImageFull__section__info__artist__follow']}>
           <button className={styles['ModalImageFull__section__info__artist__follow__button']}>Seguir</button>
           </div>
+          
+      </div>
+
+      <div className={styles['ModalImageFull__section__info__artist__description']}>
+        <p>¡Hola! Soy Ariadna, una artista digital que se dedica a crear ilustraciones personalizadas. Me encanta dibujar y pintar, y me encantaría compartir mi arte contigo. Si te gusta lo que ves, no dudes en seguirme para ver más de mi trabajo. ¡Gracias por tu apoyo! 💖</p>
       </div>
 
         {/* Comentario ficticio con nombre de usuario y texto */}
@@ -151,7 +166,7 @@ export const ModalImageFull = () => {
           <div className={styles['ModalImageFull__section__comment__name__and__username']}>
           <div className={styles['ModalImageFull__section__comment__name']}>
             <p>Lorena Mas</p>
-            <span>• 1 h</span>
+            <span>• 4 h</span>
           </div>
           <div className={styles['ModalImageFull__section__comment__username']}>
             <p>@loremas</p>
@@ -161,7 +176,89 @@ export const ModalImageFull = () => {
           <div className={styles['ModalImageFull__section__comment__text']}>
             <p>Es bellisimo por dioooooos 🙆💘</p>
             </div>
-        </div>        
+        </div>     
+        
+
+        {/* Comentario ficticio con nombre de usuario y texto */}
+        <div className={styles['ModalImageFull__section__comment__group']}>
+          <div className={styles['ModalImageFull__section__comment']}>
+            <Image
+              src='/comments/6.jpeg'
+              alt='usuario'
+              width={40}
+              height={40}
+              className={styles['ModalImageFull__section__comment__avatar']}
+            />
+          <div className={styles['ModalImageFull__section__comment__name__and__username']}>
+          <div className={styles['ModalImageFull__section__comment__name']}>
+            <p>Darla Lara</p>
+            <span>• 4 h</span>
+          </div>
+          <div className={styles['ModalImageFull__section__comment__username']}>
+            <p>@darlara</p>
+            </div>
+          </div>
+          </div>
+          <div className={styles['ModalImageFull__section__comment__text']}>
+          <p>Gracias por compartir tu arte /u\</p>
+            </div>
+        </div>
+
+        {/* Comentario ficticio con nombre de usuario y texto */}
+        <div className={styles['ModalImageFull__section__comment__group']}>
+          <div className={styles['ModalImageFull__section__comment']}>
+            <Image
+              src='/comments/7.jpeg'
+              alt='usuario'
+              width={40}
+              height={40}
+              className={styles['ModalImageFull__section__comment__avatar']}
+            />
+          <div className={styles['ModalImageFull__section__comment__name__and__username']}>
+          <div className={styles['ModalImageFull__section__comment__name']}>
+            <p>Alejandra Casas</p>
+            <span>• 1 h</span>
+          </div>
+          <div className={styles['ModalImageFull__section__comment__username']}>
+            <p>@casas_ale</p>
+            </div>
+          </div>
+          </div>
+          <div className={styles['ModalImageFull__section__comment__text']}>
+            <p>💘💘💘💘💘💘</p>
+            </div>
+        </div>     
+        
+           
+
+        {/* Comentario ficticio con nombre de usuario y texto */}
+        <div className={styles['ModalImageFull__section__comment__group']}>
+          <div className={styles['ModalImageFull__section__comment']}>
+            <Image
+              src='/comments/8.jpeg'
+              alt='usuario'
+              width={40}
+              height={40}
+              className={styles['ModalImageFull__section__comment__avatar']}
+            />
+          <div className={styles['ModalImageFull__section__comment__name__and__username']}>
+          <div className={styles['ModalImageFull__section__comment__name']}>
+            <p>Dalí P</p>
+            <span>• 1 h</span>
+          </div>
+          <div className={styles['ModalImageFull__section__comment__username']}>
+            <p>@soydalip</p>
+            </div>
+          </div>
+          </div>
+          <div className={styles['ModalImageFull__section__comment__text']}>
+            <p>Es excentrico, una delicia.</p>
+            </div>
+        </div>
+
+
+           
+           
 
 
 
