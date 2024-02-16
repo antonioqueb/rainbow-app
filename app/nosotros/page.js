d699da08f0411a668619ddca71934ff0883224b4
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-02-16 17:49:40 -0600

    Update

--- app/nosotros/page.js
+++ app/nosotros/page.js
@@ -14,11 +14,9 @@ export default function AboutPage() {
         <Image src='/ark/arktoart.jpg' width={330} height={330} alt='cover' />
       </div>
       <div className={style.container__text}>
-        <h2 className={style.container__text__title}>¿Quiénes somos?</h2>
-        <p className={style.container__text__paragraph}>Somos una empresa dedicada a la venta de productos de limpieza, para el hogar, oficina, industria, etc. Contamos con productos de calidad y precios competitivos.</p>
-        <p className={style.container__text__paragraph}>Nuestro objetivo es satisfacer las necesidades de nuestros clientes, ofreciendo productos de calidad y precios competitivos.</p>
-        <p className={style.container__text__paragraph}>Nuestro compromiso es brindar un excelente servicio, para que nuestros clientes se sientan satisfechos.</p>
-      </div>
+        <h2 className={style.container__text__title}>Nuestra Visión</h2>
+        <p className={style.container__text__paragraph}>Potenciar la venta de Arte queer y aumentar colaboraciones de artistas con empresas, ya sea del sector público o privado.</p>
+        </div>
     </section>
      <section  className={style.container_reverse}>
       <div className={style.container__img}>
@@ -27,9 +25,9 @@ export default function AboutPage() {
       </div>
       <div className={style.container__text}>
         <h2 className={style.container__text__title}>Nuestra Misión</h2>
-        <p className={style.container__text__paragraph}>Somos una empresa dedicada a la venta de productos de limpieza, para el hogar, oficina, industria, etc. Contamos con productos de calidad y precios competitivos.</p>
-        <p className={style.container__text__paragraph}>Nuestro objetivo es satisfacer las necesidades de nuestros clientes, ofreciendo productos de calidad y precios competitivos.</p>
-        <p className={style.container__text__paragraph}>Nuestro compromiso es brindar un excelente servicio, para que nuestros clientes se sientan satisfechos.</p>
+        <p className={style.container__text__paragraph}>Conectar con audiencias fuera de la de la propia comunidad queer.  </p>
+        <p className={style.container__text__paragraph}>Ser una plataforma de  referencia en Arte Contemporáneo Queer.</p>
+        <p className={style.container__text__paragraph}>Apoyar a artistas queer, sobre todo en países donde son censurados y es difícil lograr una visibilidad o difusión de su obra.</p>
       </div>
     </section>
         <section  className={style.container}>
@@ -38,24 +36,11 @@ export default function AboutPage() {
          />
       </div>
       <div className={style.container__text}>
-        <h2 className={style.container__text__title}>Nuestra Visión</h2>
-        <p className={style.container__text__paragraph}>Somos una empresa dedicada a la venta de productos de limpieza, para el hogar, oficina, industria, etc. Contamos con productos de calidad y precios competitivos.</p>
-        <p className={style.container__text__paragraph}>Nuestro objetivo es satisfacer las necesidades de nuestros clientes, ofreciendo productos de calidad y precios competitivos.</p>
-        <p className={style.container__text__paragraph}>Nuestro compromiso es brindar un excelente servicio, para que nuestros clientes se sientan satisfechos.</p>
-      </div>
+        <h2 className={style.container__text__title}>Beneficio</h2>
+        <p className={style.container__text__paragraph}>Venta de la obra de artistas queer y cerrar colaboraciones con empresas del sector públicao/privado.</p>
+       </div>
     </section>
-         <section  className={style.container_reverse}>
-      <div className={style.container__img}>
-        <Image src='/ark/valores.jpg' width={330} height={330} alt='cover'  
-         />
-      </div>
-      <div className={style.container__text}>
-        <h2 className={style.container__text__title}>Nuestros Valores</h2>
-        <p className={style.container__text__paragraph}>Somos una empresa dedicada a la venta de productos de limpieza, para el hogar, oficina, industria, etc. Contamos con productos de calidad y precios competitivos.</p>
-        <p className={style.container__text__paragraph}>Nuestro objetivo es satisfacer las necesidades de nuestros clientes, ofreciendo productos de calidad y precios competitivos.</p>
-        <p className={style.container__text__paragraph}>Nuestro compromiso es brindar un excelente servicio, para que nuestros clientes se sientan satisfechos.</p>
-      </div>
-    </section>
+
     </>
   )
 }
--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import styles from './Footer.module.css';
 import Link from 'next/link';
-import { FaFacebook, FaLinkedin } from "react-icons/fa";
+import { FaFacebook, FaLinkedin, FaTiktok } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa6";
 
@@ -23,18 +23,18 @@ export const Footer = () => {
                 </Link>
               </li>
               <li className={styles.Footer__container__footer__links__list__item}>
-                <Link href="/services" style={{ textDecoration: 'none'}}>
+                <Link href="/contacto" style={{ textDecoration: 'none'}}>
                   <span className={styles.Footer__container__footer__links__list__item__link}>Contacto</span>
                 </Link>
               </li>
               <li className={styles.Footer__container__footer__links__list__item}>
-                <Link href="/tools" style={{ textDecoration: 'none'}}>
-                  <span className={styles.Footer__container__footer__links__list__item__link}>Herramientas</span>
+                <Link href="/login" style={{ textDecoration: 'none'}}>
+                  <span className={styles.Footer__container__footer__links__list__item__link}>Login</span>
                 </Link>
               </li>
               <li className={styles.Footer__container__footer__links__list__item}>
-                <Link href="/blog"  style={{ textDecoration: 'none'}}>
-                  <span className={styles.Footer__container__footer__links__list__item__link}>Blog</span>
+                <Link href="/registro"  style={{ textDecoration: 'none'}}>
+                  <span className={styles.Footer__container__footer__links__list__item__link}>Registro</span>
                 </Link>
               </li>
 
@@ -44,68 +44,52 @@ export const Footer = () => {
             <h4 className={styles.Footer__container__footer__links__title}>Ayuda</h4>
             <ul className={styles.Footer__container__footer__links__list}>
               <li className={styles.Footer__container__footer__links__list__item}>
-                <Link href="/about" style={{ textDecoration: 'none'}}>
-                  <span className={styles.Footer__container__footer__links__list__item__link}>¿Porqué Nosotros?</span>
+                <Link href="/aviso-de-privacidad" style={{ textDecoration: 'none'}}>
+                  <span className={styles.Footer__container__footer__links__list__item__link}>Aviso de privacidad</span>
                 </Link>
               </li>
               <li className={styles.Footer__container__footer__links__list__item}>
-                <Link href="/services" style={{ textDecoration: 'none'}}>
-                  <span className={styles.Footer__container__footer__links__list__item__link}>Servicios</span>
+                <Link href="/politica-de-cookies" style={{ textDecoration: 'none'}}>
+                  <span className={styles.Footer__container__footer__links__list__item__link}>Política de cookies</span>
                 </Link>
               </li>
               <li className={styles.Footer__container__footer__links__list__item}>
-                <Link href="/tools" style={{ textDecoration: 'none'}}>
-                  <span className={styles.Footer__container__footer__links__list__item__link}>Herramientas</span>
+                <Link href="/politica-de-privacidad" style={{ textDecoration: 'none'}}>
+                  <span className={styles.Footer__container__footer__links__list__item__link}>Política de privacidad</span>
                 </Link>
               </li>
               <li className={styles.Footer__container__footer__links__list__item}>
-                <Link href="/blog" style={{ textDecoration: 'none'}}>
-                  <span className={styles.Footer__container__footer__links__list__item__link}>Blog</span>
+                <Link href="/terminos-y-condiciones" style={{ textDecoration: 'none'}}>
+                  <span className={styles.Footer__container__footer__links__list__item__link}>Terminos y Condiciones</span>
                 </Link>
               </li>
               
             </ul>
           </div>
           <div className={styles.Footer__container__row__footer__links}>
-            <h4 className={styles.Footer__container__footer__links__title}>Preguntas</h4>
+            <h4 className={styles.Footer__container__footer__links__title}>Blog</h4>
             <ul className={styles.Footer__container__footer__links__list}>
               <li className={styles.Footer__container__footer__links__list__item}>
                 <Link href="/about" style={{ textDecoration: 'none'}}>
-                  <span className={styles.Footer__container__footer__links__list__item__link}>¿Porqué Nosotros?</span>
+                  <span className={styles.Footer__container__footer__links__list__item__link}>¿Cómo comenzar si sueño con ser artista?</span>
                 </Link>
-              </li>
-              <li className={styles.Footer__container__footer__links__list__item}>
-                <Link href="/services" style={{ textDecoration: 'none'}}>
-                  <span className={styles.Footer__container__footer__links__list__item__link}>Servicios</span>
-                </Link>
-              </li>
-              <li className={styles.Footer__container__footer__links__list__item}>
-                <Link href="/tools" style={{ textDecoration: 'none'}}>
-                  <span className={styles.Footer__container__footer__links__list__item__link}>Herramientas</span>
-                </Link>
-              </li>
-              <li className={styles.Footer__container__footer__links__list__item}>
-                <Link href="/blog" style={{ textDecoration: 'none'}}>
-                  <span className={styles.Footer__container__footer__links__list__item__link}>Blog</span>
-                </Link>
-              </li>
-              
+              </li>           
             </ul>
           </div>
           <div className={styles.Footer__container__row__footer__links}>
-            <h4 className={styles.Footer__container__footer__links__title}>Siguenos</h4>
+            <h4 className={styles.Footer__container__footer__links__title}>Síguenos</h4>
             <div className={styles.Footer__container__footer__links__title__social__link}>
-              <Link href="/facebook" style={{ textDecoration: 'none'}}>
+              <Link href="https://www.facebook.com/ark.for.art" style={{ textDecoration: 'none'}}>
                 <FaFacebook className={styles.Footer__container__footer__links__title__social__link__icon} />
               </Link>
-              <Link href="/twitter" style={{ textDecoration: 'none'}}>
+              <Link href="https://twitter.com/ArkProject_Art" style={{ textDecoration: 'none'}}>
                 <FaXTwitter className={styles.Footer__container__footer__links__title__social__link__icon} />
               </Link>
-              <Link href="/instagram" style={{ textDecoration: 'none'}}>
+              <Link href="https://www.instagram.com/arkproject.mx/" style={{ textDecoration: 'none'}}>
                 <FaInstagram className={styles.Footer__container__footer__links__title__social__link__icon} />
               </Link>
-              <Link href="/linkedin" style={{ textDecoration: 'none'}}>
-                <FaLinkedin className={styles.Footer__container__footer__links__title__social__link__icon} />
+              <Link href="https://www.tiktok.com/@arkproject.mx" style={{ textDecoration: 'none'}}>
+                <FaTiktok className={styles.Footer__container__footer__links__title__social__link__icon} />
               </Link>
 
 
