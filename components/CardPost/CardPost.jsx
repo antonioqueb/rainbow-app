830dc67ffcf98e0e4e913afd6aa1cc20bc349115
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-02-16 18:13:51 -0600

    Update colors

--- components/CardPost/CardPost.jsx
+++ components/CardPost/CardPost.jsx
@@ -34,7 +34,7 @@ export const CardPost = ({ Titulo, Description, ImagePost }) => {
   }
 
   return (
-    <Link style={{textDecoration: 'none'}} href="/blog/como-comenzar-si-sueño-con-ser-artista">
+    <Link style={{textDecoration: 'none', color: 'white'}} href="/blog/como-comenzar-si-sueño-con-ser-artista">
     <div className={styles.CardPosts__container__news}>
      
         
