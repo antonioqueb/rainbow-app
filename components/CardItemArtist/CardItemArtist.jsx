54cab28e245b22c86a5a2c696fc3e90a32cf90c1
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-04-07 01:32:00 -0600

    Update

--- components/CardItemArtist/CardItemArtist.jsx
+++ components/CardItemArtist/CardItemArtist.jsx
@@ -1,30 +1,27 @@
-// CardItemArtist.js
 import React, { useState, useEffect } from 'react';
 import style from './CardItemArtist.module.css';
 import Image from 'next/image';
 import { FaEye } from "react-icons/fa";
 import { FcLike } from "react-icons/fc";
-import SkeletonCardItemArtist from './SkeletonCardItemArtist'; // Importa el componente de esqueleto
+import SkeletonCardItemArtist from './SkeletonCardItemArtist';
 
 const CardItemArtist = ({ content }) => {
   const [loading, setLoading] = useState(true);
+  const [hover, setHover] = useState(false);
 
   useEffect(() => {
-    // Simula la carga de datos
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 1); // Ajusta el tiempo según sea necesario
+    }, 1);
     return () => clearTimeout(timer);
-  }, [content]); // Dependencia para recargar si el contenido cambia
+  }, [content]);
 
   if (loading) {
     return <SkeletonCardItemArtist />;
   }
 
-  
-
   return (
-    <div className={style.slide}>
+    <div className={style.slide} onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
       <div className={style.imageWrapper}>
         <Image
           src={content.imageUrl}
@@ -33,40 +30,34 @@ const CardItemArtist = ({ content }) => {
           height={200}
           className={style.imageWrapper__img}
         />
+        {hover && (
+          <div className={style.overlay}>
+            <div className={style.profileInfo}>
+              <div className={style.profileImageWrapper}>
+                <Image
+                  className={style.profileImage}
+                  src={content.profileUrl}
+                  width={50}
+                  height={50}
+                  alt={content.profileAlt || "Profile image"}
+                />
+              </div>
+              <div className={style.profileInfo}>
+                <h2 className={style.userName}>{content.user}</h2>
+              </div>
+              <div className={style.views}>
+                <FaEye className={style.viewsIcon} />
+                <p className={style.viewsNumber}>{content.views}</p>
+              </div>
+              <div className={style.likes}>
+                <FcLike className={style.likesIcon} />
+                <p className={style.likesNumber}>{content.likes}</p>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
-
-      <div className={style.profile}>
-
-        <div className={style.profileImageWrapper}>
-          <Image
-            className={style.profileImage}
-            src={content.profileUrl}
-            width={50}
-            height={50}
-            alt={content.profileAlt || "Profile image"}
-          />
-        </div>
-
-        <div className={style.profileInfo}>
-          <div className={style.profileInfo}>
-            <h2 className={style.userName}>{content.user}</h2>
-
-          </div>
-          <div className={style.views}>
-            <FaEye className={style.viewsIcon} />
-            <p className={style.viewsNumber}>{content.views}</p>
-          </div>
-          <div className={style.likes}>
-            <FcLike className={style.likesIcon} />
-            <p className={style.likesNumber}>{content.likes}</p>
-          </div>
-        </div>
-
-      </div>
-
-
     </div>
-
   );
 };
 
--- components/CardItemArtist/SkeletonCardItemArtist.js
+++ components/CardItemArtist/SkeletonCardItemArtist.js
@@ -6,20 +6,7 @@ const SkeletonCardItemArtist = () => {
   return (
     <div className={styles.skeletonSlide}>
       <div className={styles.skeletonImageWrapper}></div>
-      <div className={styles.skeletonProfile}>
-        <div className={styles.skeletonProfileImageWrapper}></div>
-        <div className={styles.skeletonProfileInfo}>
-          <div className={styles.skeletonUserName}></div>
-          <div className={styles.skeletonMetric}>
-            <div className={styles.skeletonIcon}></div>
-            <div className={styles.skeletonText}></div>
-          </div>
-          <div className={styles.skeletonMetric}>
-            <div className={styles.skeletonIcon}></div>
-            <div className={styles.skeletonText}></div>
-          </div>
-        </div>
-      </div>
+      
     </div>
   );
 };
